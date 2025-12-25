f64b1bcd1738b761b580bc30ebb125c4cd23ed90
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-25 15:30:08 +0530

    Add Java program to find transpose of a matrix

---
+++ src/TransposeMatrix.java
@@ -0,0 +1,39 @@
+public class TransposeMatrix {
+  public static void main(String[] args) {
+    int[][] matrix = {
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9}
+    };
+
+    int[][] transpose = getTranspose(matrix);
+
+    System.out.println("Original Matrix:");
+    printMatrix(matrix);
+
+    System.out.println("\nTransposed Matrix:");
+    printMatrix(transpose);
+  }
+
+  public static int[][] getTranspose(int[][] matrix) {
+    int rows = matrix.length;
+    int cols = matrix[0].length;
+    int[][] result = new int[cols][rows];
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        result[j][i] = matrix[i][j];
+      }
+    }
+    return result;
+  }
+
+  public static void printMatrix(int[][] matrix) {
+    for (int[] row : matrix) {
+      for (int value : row) {
+        System.out.print(value + " ");
+      }
+      System.out.println();
+    }
+  }
+}
